7ca8d95f3f73ec13464080e6dad3421eab72a5e5
ind2016ltd <90252027+ind2016ltd@users.noreply.github.com>
2024-04-25 14:06:22 +0100

    Add project files.

---
+++ ExampleSetup/Decoder.cs
@@ -0,0 +1,15 @@
+﻿namespace ExampleSetup
+{
+    public static class Decoder
+    {
+        public static string EncodeForwardSlashes(string endpoint)
+        {
+            return endpoint.Replace("/", "\\").Replace(":", "colon");
+        }
+
+        public static string DecodeForwardSlashes(string endpoint)
+        {
+            return endpoint.Replace("\\", "/").Replace("colon", ":");
+        }
+    }
+}
